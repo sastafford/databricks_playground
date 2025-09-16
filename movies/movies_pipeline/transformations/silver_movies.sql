e129542b65ca00b26809498e91b8abf77fc3e2a2
sastafford <sastafford>
2025-09-16 00:14:03 +0000

    Add first version of silver tables

---
+++ movies/movies_pipeline/transformations/silver_movies.sql
@@ -0,0 +1,31 @@
+-- create the streaming table
+CREATE OR REFRESH STREAMING TABLE silver_movies;
+
+CREATE TEMPORARY VIEW silver_imdb_movies AS
+  SELECT 
+    CAST(REGEXP_REPLACE(tconst, '^tt0*', '') AS INT) as id, 
+    primaryTitle as title, 
+    split(genres, ',') as genres, 
+    startYear as year, 
+    "IMDb Non-Commercial Datasets" as source,
+    "insert" as operationType 
+  FROM STREAM(bronze_imdb_movies);
+
+
+CREATE TEMPORARY VIEW silver_mongodb_movies AS
+  SELECT 
+    imdb.id as id, 
+    wallTime,
+    title, 
+    genres, 
+    year, 
+    "mongodb_sample_mflix" as source,
+    operationType
+  FROM STREAM(bronze_mongodb_movies);
+
+CREATE FLOW mongodb_cdc_movies AS
+AUTO CDC INTO silver_movies
+FROM STREAM(silver_mongodb_movies)
+KEYS (id)
+APPLY AS DELETE WHEN operationType = "delete"
+SEQUENCE BY wallTime;
